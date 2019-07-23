9a3a97dbcf995a75ebc9a1b01fba73bf95f77e5a
Misael <misaelsarmiento97@gmail.com>
2019-07-22 22:45:00 -0500

    visuals - files
    initial upload of the files for visuals project

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+const navegacion = document.getElementById('menu')
+console.log(navegacion);
+
+navegacion.addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollBy({
+        top: 800, // could be negative value
+        left: 0,
+        behavior: 'smooth'
+
+    });
+})
